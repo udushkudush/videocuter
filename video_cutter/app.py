151b28f0d52ea6210d4ab5aa3a1d0895fd77f879
udushkudush <udushkudush@gmail.com>
2019-02-18 02:03:58 +0300

    obdade

--- video_cutter/app.py
+++ video_cutter/app.py
@@ -3,26 +3,29 @@ import subprocess
 from os.path import join, split, dirname, normpath
 import re
 import glob
+import datetime
 import json
 from PySide2 import QtCore, QtGui, QtWidgets
 from video_cutter.main_window import Ui_MainWindow
 
 os.environ['PATH'] += normpath(join(split(dirname(__file__))[0], 'ffmpeg_4.1', 'bin'))
-
+os.environ['SHOTS'] = normpath(join(split(dirname(__file__))[0], 'output'))
 
 class VideoCutter(QtWidgets.QMainWindow):
     def __init__(self):
         super(VideoCutter, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.output_path.setPlaceholderText(os.getenv('SHOTS'))
         # self.setAcceptDrops(True)
+        self.video_information = None
+        self.ui.btn_execute.setEnabled(False)
 
     # def dragEnterEvent(self, event:QtGui.QDragEnterEvent):
     def dragEnterEvent(self, event):
         if event.mimeData().hasUrls():
             event.accept()
         else:
-            print('ignore event')
             event.ignore()
 
     def dropEvent(self, event):
@@ -31,32 +34,75 @@ class VideoCutter(QtWidgets.QMainWindow):
             if os.path.isfile(path):
                 _dir, _file = split(path)
                 if _file.endswith(".mp4"):
-                    print('path: {} | file: {}'.format(_dir, _file))
                     self.analyze_video(_dir, _file)
                 else:
-                    print('path: {} | drop video: {}'.format(_dir, _file))
+                    print('This format not compatible\n\rpath: {} | drop video: {}'.format(_dir, _file))
 
     def analyze_video(self, _dir, _file):
-        mask = join(_dir, '*.EDL')
-        files = glob.glob(mask)
-        # for f in files:
-        #     print(f)
+        # путь к файлу который бум резать
         _pth = normpath(join(_dir, _file))
+        _edl = _pth.rsplit('.')[0] + '.EDL'
         _out = normpath(join(_dir, 'info.log'))
         self.ui.input_path.setText(_pth)
         cmd = 'ffprobe -i {} -v quiet -print_format json -show_format -hide_banner > {}'.format(_pth, _out)
         os.system(cmd)
-        data = ''
         text = ''
         with open(_out, 'r') as f:
-            data = json.load(f)
-        for d in data.keys():
-            c = data[d]
-            for i in c.keys():
-                if i != 'tags':
-                    text += '>> {}\t: {}\n\r'.format(i, c[i])
-                    # print('>> {}\t: {}'.format(i, c[i]))
+            self.video_information = json.load(f)
+
+        # self.video_information['edl_data'] = self.parsing_edl(_edl)
+        self.parsing_edl(_edl)
+        xx = self.video_information
+        dt = xx.get('duration_time')
+        text = 'Shots: {}\t|\tDuration: {}'.format(xx.get('shots'), dt)
+        fuck = []
+        for n,x in enumerate(xx.get('time_code')):
+            z = (dt.rsplit('.', 1)[0], x[1].rsplit('.', 1)[0])
+            if z[0] not in z[1]:
+                print(z)
+                data = (x[0], x[1])
+                a = '\n\r  sh: {} | start: {} | end: {}'.format(n + 1, x[0], x[1])
+                text += a
+                fuck.append(data)
+            elif z[0] in z[1]:
+                data = (x[0], x[1])
+                fuck.append(data)
+                a = '\n\r  sh: {} | start: {} | end: {}'.format(n + 1, x[0], x[1])
+                text += a
+                # print('blo: {}\n\rcapture: {}'.format(a, z))
+                break
         self.ui.output_info.setText(text)
+        self.ui.btn_execute.setEnabled(True)
+
+    def parsing_edl(self, _edl):
+        template = re.compile(
+            r"(^\d{3}\s+\D{2}\s+\D?\s+\D?\s+)(\d{2}:\d{2}:\d{2}:\d{2}\s?\d{2}:\d{2}:\d{2}:\d{2}\s)(\d{2}:\d{2}:\d{2}:\d{2}\s\d{2}:\d{2}:\d{2}:\d{2})\n",
+            re.MULTILINE)
+        time_group = re.compile(r"(\d{2}:\d{2}:\d{2}:\d{2})\s(\d{2}:\d{2}:\d{2}:\d{2})")
+        with open(_edl, 'r') as f:
+            _edl_data = template.findall(f.read())
+        d_time = self.video_information.get('format').get('duration')
+        t = str(datetime.timedelta(seconds=float(d_time)))
+        xx = self.video_information
+        xx['shots'] = str(len(_edl_data))
+        xx['duration_time'] = t
+        xx['time_code'] = []
+        for e in _edl_data:
+            x = time_group.findall(e[-1])[0]
+            xx['time_code'].append(('.'.join(x[0].rsplit(":", 1)), '.'.join(x[1].rsplit(":", 1))))
+
+    def cut_thish_shit(self, video):
+
+        xx = self.video_information
+        for n, x in enumerate(xx.get('time_code')):
+            _output = normpath(join(root_dir, 'output', 'out_clip_{:03d}.mp4'.format(x)))
+            _audio_out = normpath(join(root_dir, 'output', 'out_clip_{:03d}.m4a'.format(x)))
+            z = (dt.rsplit('.', 1)[0], x[1].rsplit('.', 1)[0])
+            if z[0] not in z[1]:
+                a = '\n\r  sh: {} | start: {} | end: {}'.format(n + 1, x[0], x[1])
+            elif z[0] in z[1]:
+                a = '\n\r  sh: {} | start: {} | end: {}'.format(n + 1, x[0], x[1])
+                break
 
     def video_cutter(self, video, info):
         root_dir = join(split(dirname(__file__))[0])
@@ -71,7 +117,6 @@ class VideoCutter(QtWidgets.QMainWindow):
         for x, t in enumerate(_edl_data):
             _output = normpath(join(root_dir, 'output', 'out_clip_{:03d}.mp4'.format(x)))
             _audio_out = normpath(join(root_dir, 'output', 'out_clip_{:03d}.m4a'.format(x)))
-
             if not os.path.exists(dirname(_output)):
                 print('not exists {}'.format(dirname(_output)))
                 os.makedirs(dirname(_output))
@@ -91,6 +136,7 @@ if __name__ == '__main__':
     import sys
     app = QtWidgets.QApplication(sys.argv)
     win = VideoCutter()
+    win.setGeometry(QtCore.QRect(150, 180, 450, 285))
     win.show()
     sys.exit(app.exec_())
 
--- video_cutter/main_window.py
+++ video_cutter/main_window.py
@@ -3,7 +3,7 @@
 # Form implementation generated from reading ui file 'C:\DEV\projects\videocuter\video_cutter\UI_main.ui',
 # licensing of 'C:\DEV\projects\videocuter\video_cutter\UI_main.ui' applies.
 #
-# Created: Sat Feb 16 21:54:05 2019
+# Created: Sun Feb 17 15:26:38 2019
 #      by: pyside2-uic  running on PySide2 5.12.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -13,7 +13,7 @@ from PySide2 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(471, 325)
+        MainWindow.resize(374, 259)
         MainWindow.setAcceptDrops(True)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setAcceptDrops(False)
@@ -23,18 +23,32 @@ class Ui_MainWindow(object):
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
         self.input_path = QtWidgets.QLineEdit(self.centralwidget)
+        self.input_path.setMinimumSize(QtCore.QSize(0, 24))
         self.input_path.setAcceptDrops(False)
         self.input_path.setObjectName("input_path")
         self.horizontalLayout.addWidget(self.input_path)
-        self.btn_browse = QtWidgets.QPushButton(self.centralwidget)
-        self.btn_browse.setObjectName("btn_browse")
-        self.horizontalLayout.addWidget(self.btn_browse)
+        self.btn_in_browse = QtWidgets.QPushButton(self.centralwidget)
+        self.btn_in_browse.setMinimumSize(QtCore.QSize(0, 24))
+        self.btn_in_browse.setObjectName("btn_in_browse")
+        self.horizontalLayout.addWidget(self.btn_in_browse)
         self.horizontalLayout.setStretch(0, 3)
         self.verticalLayout.addLayout(self.horizontalLayout)
         self.output_info = QtWidgets.QTextEdit(self.centralwidget)
         self.output_info.setAcceptDrops(False)
         self.output_info.setObjectName("output_info")
         self.verticalLayout.addWidget(self.output_info)
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.output_path = QtWidgets.QLineEdit(self.centralwidget)
+        self.output_path.setMinimumSize(QtCore.QSize(0, 24))
+        self.output_path.setObjectName("output_path")
+        self.horizontalLayout_2.addWidget(self.output_path)
+        self.btn_out_browse = QtWidgets.QPushButton(self.centralwidget)
+        self.btn_out_browse.setMinimumSize(QtCore.QSize(0, 24))
+        self.btn_out_browse.setObjectName("btn_out_browse")
+        self.horizontalLayout_2.addWidget(self.btn_out_browse)
+        self.horizontalLayout_2.setStretch(0, 3)
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
         self.btn_execute = QtWidgets.QPushButton(self.centralwidget)
         self.btn_execute.setMinimumSize(QtCore.QSize(0, 38))
         self.btn_execute.setObjectName("btn_execute")
@@ -49,6 +63,9 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QtWidgets.QApplication.translate("MainWindow", "MainWindow", None, -1))
-        self.btn_browse.setText(QtWidgets.QApplication.translate("MainWindow", "...", None, -1))
+        self.input_path.setPlaceholderText(QtWidgets.QApplication.translate("MainWindow", "enter path to video", None, -1))
+        self.btn_in_browse.setText(QtWidgets.QApplication.translate("MainWindow", "...", None, -1))
+        self.output_path.setPlaceholderText(QtWidgets.QApplication.translate("MainWindow", "output directory", None, -1))
+        self.btn_out_browse.setText(QtWidgets.QApplication.translate("MainWindow", "...", None, -1))
         self.btn_execute.setText(QtWidgets.QApplication.translate("MainWindow", "Cut video", None, -1))
 
